6f2ee57f1ff4361703675df6f5e914447c4b2cc6
Alex <climberfields@gmail.com>
2019-01-11 13:10:03 -0600

    added the lows to the data as well for the plot points

--- highs_lows.py
+++ highs_lows.py
@@ -6,12 +6,14 @@ from matplotlib import pyplot as plt
 
 
 #filename is the name of the file and data we are working with
-filename = 'sitka_weather_07-2014.csv'
+filename = 'sitka_weather_2014.csv'
 
 #with open opens the file (filename) as object f for the naming convention
 with open(filename) as f:
+
 # we pass f as the argument for the csv.reader which we downloaded through importing csv
     reader = csv.reader(f)
+
 #we use the next function to show us the next line in the file when passed as the reader object
     header_row = next(reader)
     print(header_row)
@@ -19,13 +21,16 @@ with open(filename) as f:
     # for index, column_header in enumerate(header_row):
     #     print(index, column_header)
 
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[0], "%Y-%m-%d")
         dates.append(current_date)
         # Take the data and convert it from a string to an integer so it can be read on matplot
         high = int(row[1])
         highs.append(high)
+
+        low = int(row[3])
+        lows.append(low)
 
 
     print(highs)
@@ -34,10 +39,12 @@ with open(filename) as f:
 
 fig = plt.figure(dpi=128, figsize=(10, 6))
 plt.plot(dates, highs, c= 'red')
+plt.plot(dates, lows, c='blue')
 fig.autofmt_xdate()
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 # Format plot.
 
-plt.title("Daily high temperatures, July 2014", fontsize=24)
+plt.title("Daily high temperatures, - 2014", fontsize=24)
 plt.xlabel('', fontsize=16)
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
